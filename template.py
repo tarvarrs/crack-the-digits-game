a6933943a6a258a5138e33615e60c04db6b5744b
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-05-10 13:20:02 +0300

    update rules, finish template

--- template.py
+++ template.py
@@ -1,42 +1,66 @@
 from random import randint as rand
 
-comp_num = '5216'
-steps = 10
+def user_input():
+    input_number = input('Введите число:')
 
-
-def is_correct_input(input_number):
-    if not isinstance(input_number, int) or input_number < 1000 or input_number > 9999:
-        return False
+    if len(input_number)!=4:
+        print('Введите четырехзначное число!')
+        user_input()
+        return
     
-    digits = [int(d) for d in str(input_number)]
-    if len(set(digits)) != 4:
-        return False
+    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     
-    return True
-    cats = check_cats(comp_num, user_num)
-    dogs = check_dogs(comp_num, user_num)
+    for digit in input_number:
+        if digit not in digits:
+            print('Введите число!')
+            user_input()
+            return
+    
+    return str(input_number)
 
-    return f'{cats} кошечек, {dogs} собачек'
 
-print(is_correct_input(int(input())))
-# def check_number():
-#     check_cats(comp_num, user_num)
-#     check_dogs(comp_num, user_num)
-#     return
-
-def check_cats(comp_num, user_num):
-    cats = 0
-    for i in range(0,4):
+def check_answer(user_num,comp_num):
+    res = ''
+    if user_num == comp_num:
+        return '🟩🟩🟩🟩\nwin win!'
+    
+    for i in range(4):
         if comp_num[i] == user_num[i]:
-            cats+=1
+            res+='🟩'
+        elif comp_num[i] in user_num:
+                res+='🟨'
+        else:
+            res+='⬜️'
 
-    return cats
+    return res
 
-def check_dogs(comp_num, user_num):
-    dogs = 0
-    for i in user_num:
-        if i in comp_num:
-            dogs += 1
+def check_win(user_num,comp_num):
+    return user_num == comp_num
+
+def mask(user_num,comp_num):
+    res = ''
+    for i in range(4):
+        if comp_num[i] == user_num[i]:
+            res += '🟩'
+        elif user_num[i] in comp_num:
+                res += '🟨'
+        else:
+            res += '⬜️'
+
+    return res
+
+#print(check_answer('1234','1234'))
+
+def game():
+    comp_num = str(rand(1000,9999))
     
-    return dogs
+    while True:
+        user_num = user_input()
+        if check_win(user_num,comp_num):
+            print('🟩🟩🟩🟩\nwin win!')
+            print(comp_num)
+            return 
+        
+        print(mask(user_num,comp_num))
 
+game()
